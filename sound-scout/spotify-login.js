006fae4f1a19d5c2830b3c375ff38d98b05ff427
a lee <alee57bc@gmail.com>
2026-03-24 11:40:55 -0400

    add inactivity timer

--- sound-scout/spotify-login.js
+++ sound-scout/spotify-login.js
@@ -10,6 +10,41 @@ document.getElementById("logout-button").addEventListener("click", async () => {
     window.location.href = "/api/logout";
 });
 
+//inactivity timer
+let inactivityTimer = null;
+const IDLE_TIMOUT_MS = 30 * 60 * 1000; //30 minutes
+
+function stopTimer(){
+    if (inactivityTimer) {
+        clearTimeout(inactivityTimer);
+        inactivityTimer = null;
+    }
+}
+
+function startTimer(){
+    stopTimer();
+
+    inactivityTimer = setTimeout(() => {
+        //logout user on inactivity
+        window.location.href = "/api/logout";
+    }, IDLE_TIMOUT_MS); 
+}
+
+function resetTimer(){
+    const loggedInSection = document.getElementById('spotify-logged-in');
+
+    //only run timer if user is logged in
+    if (loggedInSection && loggedInSection.style.display === "flex") {
+        startTimer();
+    }
+} 
+
+function allUserActivityEvents() {
+    ['click', 'mousemove', 'keydown', 'scroll', 'touchstart'].forEach(event => {
+        document.addEventListener(event, resetTimer);
+    });
+}
+
 async function checkLogin() {
     const loginSection = document.getElementById('spotify-login');
     const loggedInSection = document.getElementById('spotify-logged-in');
@@ -27,18 +62,26 @@ async function checkLogin() {
             if (loggedInSection) loggedInSection.style.display = "flex";
             if (welcomeText) {
                 welcomeText.textContent = `Welcome, ${user.name}!`;
-            }
+            } 
+
+            startTimer();
+            
         } else {
             if (loginSection) loginSection.style.display = "flex";
             if (loggedInSection) loggedInSection.style.display = "none";
+
+            stopTimer();
         } 
     } catch (err) {
         console.error("Error checking login:", err);
         if (loginSection) loginSection.style.display = "flex";
         if (loggedInSection) loggedInSection.style.display = "none";
+
+        stopTimer();
     }
 }
 
+allUserActivityEvents();
 window.addEventListener("load", checkLogin);
 
 
